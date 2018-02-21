e50c4907a6fe37bcf1b828de6db6e8fd00eedd5e
Barry McGillin <barry.mcgillin@oracle.com>
2018-02-21 12:52:51 +0000

    Adding barrys changes

---
+++ src/main/java/work/experience/Barry.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package work.experience;
+
+/**
+ * @author bamcgill
+ *
+ */
+public class Barry {
+
+	public static void main(String[] args) {
+		System.out.println("My Name is Barry");
+		barrysLoop(5);
+	}
+
+	public static void barrysLoop(int number) {
+
+		while (number < 10) {
+			System.out.println(number);
+			number++;
+		}
+
+	}
+
+}
